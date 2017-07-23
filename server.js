3f3b8b18049aa83c12742d73ab768b3f8aa96010
Bai, Yin <yin.bai@move.com>
2017-07-23 16:24:54 -0700

    add vr test route and file

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
-const http = require('http');
+'use strict'
 
+const fs = require('fs');
+const http = require ('http');
+
+const server = http.createServer();
+server.on('request', (request, response) => {
+
+    switch (request.url) {
+        case '/app.json': response.end(fs.readFileSync('./app.json'));
+        default: response.end(fs.readFileSync('./index.html'));
+    }
+
+});
+
+server.listen(process.env.PORT || 8082);
